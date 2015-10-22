80be2dcc0c9187a000b9b1d3d07ca87503c8fe8e
Simon Wunderlin <simon.wunderlin@usb.ch>
2015-10-22 15:30:15 +0200

    first working connect version

--- test/ldap.c
+++ test/ldap.c
@@ -1,37 +1,57 @@
 #include <ldap.h>
 #include <stdio.h>
 #include <string.h>
+#include "ldap.h"
 
 #define LDAP_SERVER "ldap://ms.uhbs.ch:389"
 #define LDAP_USER "muana"
 #define LDAP_PASS "anaana"
 #define LDAP_BINDDN "cn=%s,ou=GenericMove,ou=Users,ou=USB,dc=ms,dc=uhbs,dc=ch"
 
+LDAP  *ldap_connect(char* uri, char* binddn, char* bind_pw) {
+	int   rc;
+	LDAP *ld;
+	
+	//sprintf(bind_dn, LDAP_BINDDN, LDAP_USER);
+	printf("Connecting as %s\n", binddn);
+	
+	if(ldap_initialize(&ld, uri)) {
+		perror("ldap_initialize");
+		return 0;
+	}
+	
+	/*
+	printf("ldap server: %s\n", uri);
+	printf("ldap binddn: %s\n", binddn);
+	printf("ldap bindpw: %s\n", bind_pw);
+	*/
+	
+	rc = ldap_simple_bind_s(ld, binddn, bind_pw);
+	if(rc != LDAP_SUCCESS) {
+		fprintf(stderr, "ldap_simple_bind_s: %s\n", ldap_err2string(rc));
+		return 0;
+	}
+	printf( "Successfully authenticated\n" );
+	
+	return ld;
+}
+
 int main(int argc, char** argv) {
 	LDAP      *ld;
 	int       rc;
-	char      bind_dn[100];
-	struct    berval creds;
-	creds.bv_val = LDAP_PASS;
+	char      bind_dn[254];
+	
+	char url[100] = LDAP_SERVER;
 	
 	sprintf(bind_dn, LDAP_BINDDN, LDAP_USER);
-	printf( "Connecting as %s...\n", bind_dn );
- 
-	/* Open LDAP Connection */
-	if(ldap_initialize( &ld, LDAP_SERVER)) {
-		perror("ldap_initialize");
-		return(1);
+	
+ 	ld = ldap_connect(url, bind_dn, LDAP_PASS);
+	
+	if (ld != NULL) {
+		ldap_unbind_ext(ld, NULL, NULL);
+		printf("Disconnected.\n");
 	}
 	
-	/* User authentication (bind) */
-	rc = ldap_simple_bind_s(ld, bind_dn, LDAP_PASS);
-	//rc = ldap_sasl_bind_s(ld, bind_dn, LDAP_SASL_SIMPLE , &creds, NULL, NULL, NULL); //returns ldap error code 49 but continues working
-	if(rc != LDAP_SUCCESS) {
-		fprintf(stderr, "ldap_simple_bind_s: %s\n", ldap_err2string(rc) );
-		return(1);
-	}
-	
-	printf( "Successful authentication\n" );
-	ldap_unbind_ext(ld, NULL, NULL);
-	return( 0 );
+	return 0;
 }
+
---
+++ test/ldap.h
@@ -0,0 +1,2 @@
+
+LDAP *ldap_connect(char* uri, char* binddn, char* bind_pw);
