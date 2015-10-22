3dcf5417c1b909bb7bc769f329c3863db2d04168
Simon Wunderlin <simon.wunderlin@usb.ch>
2015-10-22 11:09:19 +0200

    bind example

--- test/ldap.c
+++ test/ldap.c
@@ -5,6 +5,7 @@
 #define LDAP_SERVER "ldap://ms.uhbs.ch:389"
 #define LDAP_USER "muana"
 #define LDAP_PASS "anaana"
+#define LDAP_BINDDN "cn=%s,ou=GenericMove,ou=Users,ou=USB,dc=ms,dc=uhbs,dc=ch"
 
 int main(int argc, char** argv) {
 	LDAP      *ld;
@@ -13,7 +14,7 @@ int main(int argc, char** argv) {
 	struct    berval creds;
 	creds.bv_val = LDAP_PASS;
 	
-	sprintf(bind_dn, "cn=%s,ou=GenericMove,ou=Users,dc=ms,dc=uhbs,dc=ch", LDAP_USER);
+	sprintf(bind_dn, LDAP_BINDDN, LDAP_USER);
 	printf( "Connecting as %s...\n", bind_dn );
  
 	/* Open LDAP Connection */
@@ -23,8 +24,8 @@ int main(int argc, char** argv) {
 	}
 	
 	/* User authentication (bind) */
-	//rc = ldap_simple_bind_s(ld, bind_dn, LDAP_PASS);
-	rc = ldap_sasl_bind_s(ld, bind_dn, LDAP_SASL_SIMPLE , &creds, NULL, NULL, NULL); //returns ldap error code 49 but continues working
+	rc = ldap_simple_bind_s(ld, bind_dn, LDAP_PASS);
+	//rc = ldap_sasl_bind_s(ld, bind_dn, LDAP_SASL_SIMPLE , &creds, NULL, NULL, NULL); //returns ldap error code 49 but continues working
 	if(rc != LDAP_SUCCESS) {
 		fprintf(stderr, "ldap_simple_bind_s: %s\n", ldap_err2string(rc) );
 		return(1);
