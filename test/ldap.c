3dc771937753809a67fb2d362d358e82c8426e13
Simon Wunderlin <simon.wunderlin@usb.ch>
2015-10-22 16:48:57 +0200

    printing result

--- test/ldap.c
+++ test/ldap.c
@@ -1,3 +1,8 @@
+/*
+	LDAP Search example
+	Reference: http://www-archive.mozilla.org/directory/csdk-docs/search.htm
+*/
+
 #include <ldap.h>
 #include <stdio.h>
 #include <string.h>
@@ -7,6 +12,8 @@
 #define LDAP_USER "muana"
 #define LDAP_PASS "anaana"
 #define LDAP_BINDDN "cn=%s,ou=GenericMove,ou=Users,ou=USB,dc=ms,dc=uhbs,dc=ch"
+#define LDAP_SEARCH_BASE "ou=USB,dc=ms,dc=uhbs,dc=ch"
+#define LDAP_GROUP_FILTER "(&(objectClass=group)(cn=*B_M_Anaesthesiologie*))"
 
 LDAP  *ldap_connect(char* uri, char* binddn, char* bind_pw) {
 	int   rc;
@@ -36,17 +43,92 @@ LDAP  *ldap_connect(char* uri, char* binddn, char* bind_pw) {
 	return ld;
 }
 
+void ldap_display_attributes(LDAP *ld, LDAPMessage *msg) {
+	// get all attributes
+	char *a;
+	BerElement *ber;
+	
+	for (a = ldap_first_attribute(ld, msg, &ber); 
+		a != NULL; 
+		a = ldap_next_attribute(ld, msg, ber)) {
+		
+		char **vals;
+		int i;
+		// Get the values of the attribute. 
+		if ( ( vals = ldap_get_values( ld, msg, a ) ) != NULL ) {
+			for ( i = 0; vals[i] != NULL; i++ ) {
+				// Print the name of the attribute and each value
+				printf( "%s: %s\n", a, vals[i] );
+			}
+			// Free the attribute values from memory when done.
+			ldap_value_free(vals);
+		}
+	}
+	ldap_memfree(a);
+}
+
 int main(int argc, char** argv) {
 	LDAP      *ld;
 	int       rc;
 	char      bind_dn[254];
+	LDAPMessage *res, *msg;
+	int msgtype;
+	char *dn;
 	
-	char url[100] = LDAP_SERVER;
-	
+	char url[254] = LDAP_SERVER;
 	sprintf(bind_dn, LDAP_BINDDN, LDAP_USER);
 	
  	ld = ldap_connect(url, bind_dn, LDAP_PASS);
 	
+	// fetch result
+	rc = ldap_search_s(ld, LDAP_SEARCH_BASE, LDAP_SCOPE_SUBTREE, LDAP_GROUP_FILTER, NULL, 0, &res);
+	if (rc != LDAP_SUCCESS) {
+		if (ld != NULL) {
+			ldap_unbind_ext(ld, NULL, NULL);
+			printf("Disconnected.\n");
+		}
+		
+		fprintf(stderr, "ldap_simple_bind_s: %s\n", ldap_err2string(rc));
+		return 0;
+	}
+	
+	// loop over result
+	int num_entries = 0;
+	int num_refs = 0;
+	
+	num_entries = ldap_count_entries( ld, res );
+	num_refs = ldap_count_references( ld, res );
+	printf("Number of entried: %d, number of References: %d\n", num_entries, num_refs);
+	
+	for ( msg = ldap_first_message( ld, res ); 
+		msg != NULL; 
+		msg = ldap_next_message( ld, msg ) ) {
+
+		/* Determine what type of message was sent from the server. */
+		msgtype = ldap_msgtype( msg );
+		switch( msgtype ) {
+			case LDAP_RES_SEARCH_ENTRY:
+				// The result is an entry, print dn
+				if ( ( dn = ldap_get_dn( ld, msg ) ) != NULL ) {
+					printf( "==================================================\n");
+					printf( "dn: %s\n", dn );
+					/* Free the memory used for the DN when done */
+					ldap_memfree( dn );
+				}
+				
+				ldap_display_attributes(ld, msg);
+				
+				break;
+			case LDAP_RES_SEARCH_REFERENCE:
+				/* The result is a search reference. */
+				break;
+			case LDAP_RES_SEARCH_RESULT:
+				/* The result is the final result sent by the server. */
+				break;
+		}
+	}
+	
+	// disconnect
 	if (ld != NULL) {
 		ldap_unbind_ext(ld, NULL, NULL);
 		printf("Disconnected.\n");
--- test/ldap.h
+++ test/ldap.h
@@ -1,2 +1,4 @@
 
 LDAP *ldap_connect(char* uri, char* binddn, char* bind_pw);
+void ldap_display_attributes(LDAP *ld, LDAPMessage *msg);
+
