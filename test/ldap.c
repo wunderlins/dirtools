43ce447a3c80bb3ab0188173e0e6349bd82fe8fe
Simon Wunderlin <simon.wunderlin@usb.ch>
2015-10-23 11:10:27 +0200

    added LDConn struct and guid parsing examples

--- test/ldap.c
+++ test/ldap.c
@@ -6,7 +6,9 @@
 #include <ldap.h>
 #include <stdio.h>
 #include <string.h>
-#include <wchar.h>
+#include <stdint.h>
+#include <stdlib.h>
+
 #include "ldap.h"
 
 #define LDAP_SERVER "ldap://ms.uhbs.ch:389"
@@ -16,7 +18,17 @@
 #define LDAP_SEARCH_BASE "ou=USB,dc=ms,dc=uhbs,dc=ch"
 #define LDAP_GROUP_FILTER "(&(objectClass=group)(cn=*B_M_Anaesthesiologie*))"
 
-LDAP  *ldap_connect(char* uri, char* binddn, char* bind_pw) {
+typedef struct ldconn {
+	LDAP *ld;
+	char *binddn;
+	char *uri;
+	char *pass;
+	char *search_base;
+	char *search_filter;
+	LDAPMessage *res;
+} LDConn;
+
+LDAP *ldap_connect(char* uri, char* binddn, char* bind_pw) {
 	int   rc;
 	LDAP *ld;
 	
@@ -44,13 +56,78 @@ LDAP  *ldap_connect(char* uri, char* binddn, char* bind_pw) {
 	return ld;
 }
 
-char *guid2str(char *raw) {
+/**
+ * fixme: adressing wrong bytes I guess
+ * http://stackoverflow.com/questions/1672677/print-a-guid-variable
+ * https://packages.debian.org/jessie/libuuid1
+ */
+char *guid2str(const unsigned char *raw) {
+	/*
+	uuid.h
+
+	#include "copyrt.h"
+	#undef uuid_t
+	typedef struct {
+		  unsigned32  time_low;
+		  unsigned16  time_mid;
+		  unsigned16  time_hi_and_version;
+		  unsigned8   clock_seq_hi_and_reserved;
+		  unsigned8   clock_seq_low;
+		  byte        node[6];
+	} uuid_t;
+	
+	// puid -- print a UUID 
+	void puid(uuid_t u)
+	{
+		  int i;
+
+		  printf("%8.8x-%4.4x-%4.4x-%2.2x%2.2x-", u.time_low, u.time_mid,
+		  u.time_hi_and_version, u.clock_seq_hi_and_reserved,
+		  u.clock_seq_low);
+		  for (i = 0; i < 6; i++)
+		      printf("%2.2x", u.node[i]);
+		  printf("\n");
+	}
+	typedef struct {
+		  uint32_t  time_low;
+		  uint16_t  time_mid;
+		  uint16_t  time_hi_and_version;
+		  uint8_t   clock_seq_hi_and_reserved;
+		  uint8_t   clock_seq_low;
+		  uint8_t   node[6];
+	} uuid_t;
+	
+	uuid_t *u;
+	u = malloc(sizeof(uuid_t));
+	memcpy((void *) u->time_low, (const void *) raw[0], 4);
+	memcpy((void *) u->time_mid, (char *) raw[4], 2);
+	memcpy((void *) u->time_hi_and_version, (char *) raw[6], 2);
+	memcpy((void *) u->clock_seq_hi_and_reserved, (char *) raw[8], 1);
+	memcpy((void *) u->clock_seq_low, (char *) raw[9], 1);
+	
+	u->node[0] = raw[10];
+	u->node[1] = raw[11];
+	u->node[2] = raw[12];
+	u->node[3] = raw[13];
+	u->node[4] = raw[14];
+	u->node[5] = raw[15];
+	*/
+	
 	char str[40];
+	
 	sprintf(str, "{%08lX-%04hX-%04hX-%02hhX%02hhX-%02hhX%02hhX%02hhX%02hhX%02hhX%02hhX}",
 		raw[3], raw[2], raw[1], raw[0], raw[5], raw[4],
 		raw[7], raw[6], raw[8], raw[9], raw[10], raw[11],
 		raw[12], raw[13], raw[14], raw[15]
 	);
+	
+	/*
+	sprintf(str, 
+		"%8.8x-%4.4x-%4.4x-%2.2x%2.2x-%2.2x%2.2x%2.2x%2.2x%2.2x%2.2x", u->time_low, u->time_mid,
+		  u->time_hi_and_version, u->clock_seq_hi_and_reserved,
+		  u->clock_seq_low, u->node[0], u->node[1], u->node[2], u->node[3], u->node[4], u->node[5]
+	);
+	*/
 	
 	return str;
 }
@@ -63,6 +140,9 @@ void ldap_display_attributes(LDAP *ld, LDAPMessage *msg) {
 	for (a = ldap_first_attribute(ld, msg, &ber); 
 		a != NULL; 
 		a = ldap_next_attribute(ld, msg, ber)) {
+		
+		// skip all non objectGUID attributes for testing
+		// if (strcmp(a, "objectGUID") != 0) continue; 
 		
 		char **vals;
 		int i;
@@ -93,17 +173,28 @@ int main(int argc, char** argv) {
 	LDAPMessage *res, *msg;
 	int msgtype;
 	char *dn;
+	LDConn *conn;
 	
-	char url[254] = LDAP_SERVER;
-	sprintf(bind_dn, LDAP_BINDDN, LDAP_USER);
+	// setup our connection data structure
+	sprintf(conn->binddn, LDAP_BINDDN, LDAP_USER);
+	conn->pass = LDAP_PASS;
+	conn->search_base = LDAP_SEARCH_BASE;
+	conn->search_filter = LDAP_GROUP_FILTER;
+	conn->uri = LDAP_SERVER;
 	
- 	ld = ldap_connect(url, bind_dn, LDAP_PASS);
+	// prepare the connection
+ 	conn->ld = ldap_connect(conn->uri, conn->binddn, conn->pass);
+ 	if (conn->ld == 0) {
+		fprintf(stderr, "failed to connect, aborting.\n");
+ 		return 1;
+ 	}
 	
 	// fetch result
-	rc = ldap_search_s(ld, LDAP_SEARCH_BASE, LDAP_SCOPE_SUBTREE, LDAP_GROUP_FILTER, NULL, 0, &res);
+	rc = ldap_search_s(conn->ld, conn->search_base, LDAP_SCOPE_SUBTREE, 
+	                   conn->search_filter, NULL, 0, &(conn->res));
 	if (rc != LDAP_SUCCESS) {
 		if (ld != NULL) {
-			ldap_unbind_ext(ld, NULL, NULL);
+			ldap_unbind_ext(conn->ld, NULL, NULL);
 			printf("Disconnected.\n");
 		}
 		
@@ -115,27 +206,27 @@ int main(int argc, char** argv) {
 	int num_entries = 0;
 	int num_refs = 0;
 	
-	num_entries = ldap_count_entries( ld, res );
-	num_refs = ldap_count_references( ld, res );
+	num_entries = ldap_count_entries(conn->ld, conn->res);
+	num_refs = ldap_count_references(conn->ld, conn->res);
 	printf("Number of entried: %d, number of References: %d\n", num_entries, num_refs);
 	
-	for ( msg = ldap_first_message( ld, res ); 
+	for ( msg = ldap_first_message( conn->ld, conn->res ); 
 		msg != NULL; 
-		msg = ldap_next_message( ld, msg ) ) {
+		msg = ldap_next_message( conn->ld, msg ) ) {
 
 		/* Determine what type of message was sent from the server. */
-		msgtype = ldap_msgtype( msg );
+		msgtype = ldap_msgtype(msg);
 		switch( msgtype ) {
 			case LDAP_RES_SEARCH_ENTRY:
 				// The result is an entry, print dn
-				if ( ( dn = ldap_get_dn( ld, msg ) ) != NULL ) {
+				if ( ( dn = ldap_get_dn( conn->ld, msg ) ) != NULL ) {
 					printf( "==================================================\n");
 					printf( "dn: %s\n", dn );
 					/* Free the memory used for the DN when done */
 					ldap_memfree( dn );
 				}
 				
-				ldap_display_attributes(ld, msg);
+				ldap_display_attributes(conn->ld, msg);
 				
 				break;
 			case LDAP_RES_SEARCH_REFERENCE:
@@ -148,8 +239,8 @@ int main(int argc, char** argv) {
 	}
 	
 	// disconnect
-	if (ld != NULL) {
-		ldap_unbind_ext(ld, NULL, NULL);
+	if (conn->ld != NULL) {
+		ldap_unbind_ext(conn->ld, NULL, NULL);
 		printf("Disconnected.\n");
 	}
 	
--- test/ldap.h
+++ test/ldap.h
@@ -1,4 +1,5 @@
 
+
 LDAP *ldap_connect(char* uri, char* binddn, char* bind_pw);
 void ldap_display_attributes(LDAP *ld, LDAPMessage *msg);
 
