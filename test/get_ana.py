aea4df18b2cb24fee42263960a7a118a773b044c
Simon Wunderlin <simon.wunderlin@usb.ch>
2015-10-29 20:35:30 +0100

    added

---
+++ test/get_ana.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+
+import ldap, ldif, sys, re
+import pprint
+from datetime import datetime
+import ldap.filter
+import csv
+
+config = {
+	"host": "ms.uhbs.ch",
+	"port": None,
+	"user": "muana",
+	"pass": "anaana",
+	"base": "dc=ms,dc=uhbs,dc=ch", # where to look for computers
+	"search_filter": "(objectClass=user)", # search for members in these groups
+	"vdlist" : "import/VDsMQAssignedUser-10-15.csv"
+};
+
+
+def get_ma():
+	l = None
+	if config["port"]:
+		l = ldap.open(config["host"], config["port"])
+	else:
+		l = ldap.open(config["host"])
+	
+	l.protocol_version = ldap.VERSION3
+	username = "cn=" + config["user"] +",ou=GenericMove,ou=Users,ou=USB,dc=ms,dc=uhbs,dc=ch"
+	l.simple_bind_s(username, config["pass"])
+	
+	#print l
+	
+	"""
+	Get All users from:
+	- CN=MQ_ANA_AA,OU=Exchange_Adressbuecher,OU=PITServer,DC=ms,DC=uhbs,DC=ch
+	- CN=MQ_ANA_AA,OU=Exchange_Adressbuecher,OU=PITServer,DC=ms,DC=uhbs,DC=ch
+	- CN=MQ_ANA_AA,OU=Exchange_Adressbuecher,OU=PITServer,DC=ms,DC=uhbs,DC=ch
+	"""
+	cnlist = []
+	
+	# find groups of interest
+	f = "(|(cn=MQ_ANA_AA)(cn=MQ_ANA_OA)(cn=MQ_ANA_LA))"
+	base = "OU=Exchange_Adressbuecher,OU=PITServer,"+config["base"]
+	res = l.search_s(base, ldap.SCOPE_SUBTREE, f)
+	for r in res:
+		#print r[0]
+		cnlist = cnlist + r[1]["member"]
+		#group_filter += ")(memberOf=" + r[0]
+	
+	result = []
+	aduser = []
+	dnlist = []
+	#cnlist = ["cn=wunderlin*"]
+	for u in cnlist:
+		#f = u.replace("\\", "\\\\")
+		#f = u.replace("\\,", "*")
+		#f = f.replace("(", "")
+		#f = f.replace(")", "")
+		#f = ldap.filter.escape_filter_chars(u, 0)#.replace("\\5c", "\\", 1)
+		#f = u.replace("\\,", "*")
+		f = re.sub(r'([^\\]),.*', "\\1", u)
+		f = f.replace("\\,", ",")
+		cn = f
+		dn = u
+		f = ldap.filter.escape_filter_chars(f, 0);
+		
+		res = l.search_s("OU=USB,"+config["base"], ldap.SCOPE_SUBTREE, f)
+		samaccountname = None
+		try:
+			#print res[0][1]["sAMAccountName"][0]
+			samaccountname = res[0][1]["sAMAccountName"][0]
+		except:
+			pass
+		
+		result.append( (u, cn, samaccountname) )
+		aduser.append(samaccountname)
+		dnlist.append(dn)
+		
+	#print cnlist
+	#print aduser
+	
+	vdlist = []
+	#read csv file
+	with open(config["vdlist"], 'rb') as csvfile:
+		hostreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+		for row in hostreader:
+			adname = ""
+			try:
+				adname = row[3].split("\\")[1]
+			except:
+				pass
+				
+			if adname not in aduser:
+				continue
+			
+			ix = aduser.index(adname)
+			print row[0], " ", adname, dnlist[ix]
+			
+	
+def main():
+	get_ma()
+
+if __name__ == "__main__":
+	main()
+
