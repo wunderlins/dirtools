06bde2681cf39082a4cf11fce99f5acb69bb7972
Simon Wunderlin <simon.wunderlin@usb.ch>
2015-10-30 09:25:01 +0100

    cleanup

--- bin/get_ana.py
+++ bin/get_ana.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import os
 import ldap, ldif, sys, re
 import pprint
 from datetime import datetime
@@ -21,32 +22,10 @@ config = {
 def get_ma():
 	l = None
 	
-	"""
-	if config["port"]:
-		l = ldap.open(config["host"], config["port"])
-	else:
-		l = ldap.open(config["host"])
-	
-	l.protocol_version = ldap.VERSION3
-	username = "cn=" + config["user"] +",ou=GenericMove,ou=Users,ou=USB,dc=ms,dc=uhbs,dc=ch"
-	l.simple_bind_s(username, config["pass"])
-	"""
-	
-	
-	
 	l = ldap.initialize(config["uri"])
 	l.protocol_version = ldap.VERSION3
-	#print config["uri"]
-	#print config["binddn"]
-	#print config["pass"]
 	l.simple_bind_s(config["binddn"], config["pass"])
 	
-	"""
-	Get All users from:
-	- CN=MQ_ANA_LA,OU=Exchange_Adressbuecher,OU=PITServer,DC=ms,DC=uhbs,DC=ch
-	- CN=MQ_ANA_OA,OU=Exchange_Adressbuecher,OU=PITServer,DC=ms,DC=uhbs,DC=ch
-	- CN=MQ_ANA_AA,OU=Exchange_Adressbuecher,OU=PITServer,DC=ms,DC=uhbs,DC=ch
-	"""
 	cnlist = []
 	
 	# find groups of interest
@@ -54,21 +33,12 @@ def get_ma():
 	base = "OU=Exchange_Adressbuecher,OU=PITServer,"+config["base"]
 	res = l.search_s(base, ldap.SCOPE_SUBTREE, f)
 	for r in res:
-		#print r[0]
 		cnlist = cnlist + r[1]["member"]
-		#group_filter += ")(memberOf=" + r[0]
 	
-	result = []
-	aduser = []
-	dnlist = []
-	#cnlist = ["cn=wunderlin*"]
+	result = [] # list of (user, cn, sAMAccountName)
+	aduser = [] # list of sAMAccountName
+	dnlist = [] # all dn with a sAMAccountName
 	for u in cnlist:
-		#f = u.replace("\\", "\\\\")
-		#f = u.replace("\\,", "*")
-		#f = f.replace("(", "")
-		#f = f.replace(")", "")
-		#f = ldap.filter.escape_filter_chars(u, 0)#.replace("\\5c", "\\", 1)
-		#f = u.replace("\\,", "*")
 		f = re.sub(r'([^\\]),.*', "\\1", u)
 		f = f.replace("\\,", ",")
 		cn = f
@@ -92,7 +62,7 @@ def get_ma():
 	
 	vdlist = []
 	#read csv file
-	with open(config["vdlist"], 'rb') as csvfile:
+	with open(basepath + "../var/" + config["vdlist"], 'rb') as csvfile:
 		hostreader = csv.reader(csvfile, delimiter=',', quotechar='"')
 		for row in hostreader:
 			adname = ""
@@ -113,17 +83,17 @@ def get_ma():
 				pass
 			
 			ix = aduser.index(adname)
-			print vddn, ";", dnlist[ix]
+			print "%s;%s" % (vddn, dnlist[ix])
 			
-	
 def main():
 	get_ma()
 
 if __name__ == "__main__":
 	
 	# read password
+	basepath = os.path.dirname(os.path.realpath(__file__)) + "/"
 	hostname = socket.gethostname()
-	passfile = '../etc/%s.pass' % hostname 
+	passfile = basepath+'../etc/%s.pass' % hostname 
 	try:
 		with open(passfile) as f: 
 			config["pass"] = f.read()
@@ -134,7 +104,7 @@ if __name__ == "__main__":
 	
 	# read config
 	def read_cfg():
-		sys.path.append('../etc/')
+		sys.path.append(basepath+'../etc/')
 		try:
 			#from shell1 import *
 			cfgfile = __import__(hostname)
@@ -148,4 +118,4 @@ if __name__ == "__main__":
 	read_cfg()
 	
 	main()
-
+	
